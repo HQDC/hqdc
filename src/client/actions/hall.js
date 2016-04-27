58d33538e9adb1d215202544e73ab25e4d310f2d
vsheyan <vsheyan@gmail.com>
2016-04-27 23:28:17 +0800

    create modal

--- src/client/actions/hall.js
+++ src/client/actions/hall.js
@@ -5,7 +5,6 @@ import ModalTypes from '../constants/ModalTypes';
 import CreateRoomTestModal from '../components/CreateRoomTestModal';
 export function showCreateRoom() {
 	return (dispatch, getState) => {
-		console.log("showCreateRoom");
 		dispatch({
 			type: ModalTypes.SHOW_MODAL,
 			modal: CreateRoomTestModal
--- src/client/containers/ModalContainer.js
+++ src/client/containers/ModalContainer.js
@@ -1,25 +1,17 @@
 /**
  * Created by Tile on 2015/12/1.
  */
-import React, {
-    PropTypes, Component
-}
-from 'react';
-import {
-    delModal
-}
-from '../actions/modal';
-import {
-    connect
-}
-from 'react-redux';
-import {
-    Alert
-}
-from 'react-bootstrap';
+import React, {PropTypes, Component}from 'react';
+import {delModal}from '../actions/modal';
+import {connect} from 'react-redux';
+import {Alert} from 'react-bootstrap';
+import CreateRoomTestModal from '../components/CreateRoomTestModal';
 class ModalContainer extends Component {
     render() {
-        let showModal = this.props.curtmodal;
+        let showModal = "";
+        if (this.props.curtmodal != null && this.props.curtmodal != ""){
+            showModal = React.createElement(this.props.curtmodal,null);
+        }
         console.log("ModalContainer render", showModal);
         return (
             <div className="modalContainer">
